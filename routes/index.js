c8dec39cf914f59761897abb38848017d5b8c1e1
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-03-15 22:42:26 +0530

    Initial google maps addition

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let keys = require('../config/keys')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index_ejs', { title: 'Express' });
+  console.log(keys)
+  res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_maps_api_key') });
 });
 
 router.get('/about', function(req, res, next) {

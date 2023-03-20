17ce159583c93ed3f54a3d55b9f500ed235eaead
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-03-20 22:53:55 +0530

    initial sequelize set up with some errors

---
+++ models/Users.js
@@ -0,0 +1,11 @@
+// import { Sequelize, Model, DataTypes } from 'sequelize';
+
+// const sequelize = new Sequelize({
+//     dialect: "sqlite",
+//     storage: "../db/db.sqlite"
+// });
+// const User = sequelize.define('users', {
+//   id: DataTypes.NUMBER,
+//   name: DataTypes.STRING,
+// });
+// module.exports = User;

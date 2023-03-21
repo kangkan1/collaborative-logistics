b9717d4cdc7bbd288bad60e795a2d4619a96a019
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-03-21 14:55:55 +0530

    favison added, db configured for sequalize

---
+++ models/users.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize( {
+  dialect: 'sqlite',
+  storage: './db/db.sqlite'
+});
+const User = sequelize.define('users', {
+    id: {
+      type: Sequelize.NUMBER,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING
+    }
+  });
+  module.exports = User;
+  
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
 let keys = require('../config/keys')
+let User = require('../models/users')
+
+const sqlite3 = require('sqlite3').verbose();
+const { promisify } = require('util');
+const db = new sqlite3.Database('./db/db.sqlite');
+const all = promisify(db.all).bind(db);
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize( {
+  dialect: 'sqlite',
+  storage: './db/db.sqlite'
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(keys)
-  res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_maps_api_key') });
+  User.create({
+    id:5,
+    name:"Kunal"
+  }).then((user) => console.log(user.toJSON()))
+  .catch((error) => console.error(error));
+  db.all('SELECT * FROM users', (err, rows) => {
+    if (err) {
+      console.error(err);
+      // res.status(500).send('Server Error');
+      res.render('error', {status:500})
+      return;
+    }
+    res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_map_api_key'), users:rows });
+  });
+  // res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_map_api_key') });
 });
 
 router.get('/about', function(req, res, next) {

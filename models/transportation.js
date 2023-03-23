bcc63e05eb6588de9c42dfd4ad6610057c23cfe7
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-03-23 15:59:28 +0530

    Initial configuration and saving transportation

--- models/transportation.js
+++ models/transportation.js
@@ -4,19 +4,51 @@ const sequelize = new Sequelize( {
   storage: './db/db.sqlite'
 });
 
-// const Transportation = sequelize.define('users', {
-//     id: {
-//       type: Sequelize.NUMBER,
-//       primaryKey: true
-//     },
-//     departure_time: {
-//       type: Sequelize.DATE
-//     }
-//     arrival_time: {
-//         type: Sequelize.DATE
-//       }
-//   },{ timestamps: false });
-//   module.exports = User;
+const Transportation = sequelize.define('transportation', {
+    id: {
+      type: Sequelize.NUMBER,
+      primaryKey: true
+    },
+    departure_time: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    arrival_time: {
+        type: Sequelize.DATE
+	  },
+    _from: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    _to: {
+      type: Sequelize.STRING(128),
+      allowNull: false
+    }, 
+    other_stopping_details: {
+      type: Sequelize.STRING(1024)
+    },
+    details: {
+      type: Sequelize.STRING(1024)
+    },
+    occupancy_unit: {
+      type: Sequelize.STRING(128)
+    },
+    occupancy_number: {
+      type: Sequelize.STRING(128)
+    },
+    vehicle_details: {
+      type: Sequelize.STRING(128)
+    },
+    has_freezer: {
+      type: Sequelize.STRING(5)
+    },
+    rider_name: {
+      type: Sequelize.STRING(128)
+    }, 
+
+
+  },{ timestamps: false });
+  module.exports = Transportation;
 
   /*
     Create statement
@@ -31,9 +63,9 @@ const sequelize = new Sequelize( {
    	details text(1024),
    	occupancy_unit text(128),
    	occupancy_number text(15),
-	vehicle_details text(128),
-	has_freezer text(5),
-	rating DECIMAL(10,3),
-	rider_name text(128)
+    vehicle_details text(128),
+    has_freezer text(5),
+    rating DECIMAL(10,3),
+    rider_name text(128)
 );
   */
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let keys = require('../config/keys')
 let User = require('../models/users')
+let Transportation = require('../models/transportation')
 
 const sqlite3 = require('sqlite3').verbose();
 const { promisify } = require('util');
@@ -17,6 +18,20 @@ const sequelize = new Sequelize( {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  Transportation.create({
+    departure_time: new Date(),
+    arrival_time: new Date(),
+    _from: "New Delhi",
+    _to: "Kurukshetra",
+    other_stopping_details: "Karnal,Panipat",
+    details: "This is pick up van. It is usually used for transportation of vegetables. It doesn't have freezer",
+    occupancy_unit: "kg",
+    occupancy_number:"1200",
+    vehicle_details: "Mahindra Pick Up van, model 121",
+    has_freezer: "No",
+    rider_name:"Kunal"
+  }).then((user) => console.log(user.toJSON()))
+  .catch((error) => console.error(error));
   // User.create({
   //   id:5,
   //   name:"Kunal"
@@ -29,7 +44,8 @@ router.get('/', function(req, res, next) {
       res.render('error', {status:500})
       return;
     }
-    res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_maps_api_key'), users:rows });
+    console.log(keys)
+    res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_map_api_key'), users:rows });
   });
   // res.render('index_ejs', { title: 'Collaborative Logistics', api_key:keys.get('google_map_api_key') });
 });

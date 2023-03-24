57e5686327d0ce86c2bfdd37376a761e1624df3c
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-03-24 22:23:05 +0530

    transportation page configuration

--- routes/index.js
+++ routes/index.js
@@ -24,20 +24,22 @@ const sequelize = new Sequelize( {
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Transportation.create({
-    departure_time: new Date(),
-    arrival_time: new Date(),
-    _from: "New Delhi",
-    _to: "Kurukshetra",
-    other_stopping_details: "Karnal,Panipat",
-    details: "This is pick up van. It is usually used for transportation of vegetables. It doesn't have freezer",
-    occupancy_unit: "kg",
-    occupancy_number:"1200",
-    vehicle_details: "Mahindra Pick Up van, model 121",
-    has_freezer: "No",
-    rider_name:"Kunal"
-  }).then((user) => console.log(user.toJSON()))
-  .catch((error) => console.error(error));
+  // Transportation.create({
+  //   departure_time: new Date(),
+  //   arrival_time: new Date(),
+  //   _from: "New Delhi",
+  //   _to: "Kurukshetra",
+  //   other_stopping_details: "Karnal,Panipat",
+  //   details: "This is pick up van. It is usually used for transportation of vegetables. It doesn't have freezer",
+  //   occupancy_unit: "kg",
+  //   occupancy_number:"1200",
+  //   vehicle_details: "Mahindra Pick Up van, model 121",
+  //   has_freezer: "No",
+  //   rider_name:"Kunal"
+  // }).then((user) => console.log(user.toJSON()))
+  // .catch((error) => console.error(error));
+
+
   // User.create({
   //   id:5,
   //   name:"Kunal"
@@ -74,6 +76,21 @@ router.get('/maps', function(req, res, next) {
     res.render('maps', { title: 'Maps', api_key:keys.get('google_maps_api_key'), users:rows });
   });
   //res.render('maps', { title: 'Maps', api_key:keys.get('google_map_api_key'), users:rows });
+});
+
+router.get('/transportation', function(req, res, next) {
+  db.all('SELECT * FROM transportation;', (err, rows) => {
+    if (err) {
+      console.error(err);
+      // res.status(500).send('Server Error');
+      res.render('error', {status:500})
+      return;
+    }
+
+    // console.log(keys)
+    res.render('transportation', { title: 'Collaborative Logistics | Transportation', transportation:rows });
+  });
+  // res.render('transportation', { title: 'transportation' });
 });
 
 
